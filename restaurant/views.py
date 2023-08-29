f0a7a260bcccd46eca7ff2d65ce511c1565306de
mahdifehri <mahdi.fehri@esprit.tn>
2023-08-29 16:53:36 +0200

    add-menu-API

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, DestroyAPIView
+from .models import Booking, MenuItem
+from rest_framework.viewsets import ModelViewSet
+from .serializers import BookingSerializer, MenuItemSerializer
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'index.html', {})
+
+
+class MenuItemsView(ListCreateAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+
+
+class SingleMenuItemView(RetrieveUpdateAPIView, DestroyAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+
+
+class BookingViewSet(ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    permission_classes = [IsAuthenticated]
